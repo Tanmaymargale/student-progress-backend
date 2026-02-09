28eb29daea7766034c8ff84f857fed159f03cdf1
Tanmay Margale <tanmaymargale@gmail.com>
2026-02-09 13:40:37 +0530

    changes in sheet.py file

--- sheets.py
+++ sheets.py
@@ -1,20 +1,25 @@
 import os
-from oauth2client.service_account import ServiceAccountCredentials
+import json
 import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 scope = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/drive"
 ]
 
-# Absolute path
-base_dir = os.path.dirname(__file__)
-json_path = os.path.join(base_dir, "service_account.json")
+# ✅ Load credentials from Render environment variable
+service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(json_path, scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(
+    service_account_info,
+    scope
+)
+
 client = gspread.authorize(creds)
 
 sheet = client.open("Project_Progress_Management")
+
 students_ws = sheet.worksheet("students")
 batches_ws = sheet.worksheet("batches")
 assignment_ws = sheet.worksheet("assignment")
